417c6101e0ef713ff59d353387c82955cd6cb0cf
Flavio Castelli <fcastelli@suse.com>
2023-11-21 13:30:01 +0100

    add dotnet template repo
    Signed-off-by: Flavio Castelli <fcastelli@suse.com>

--- terraform/repository_others.tf
+++ terraform/repository_others.tf
@@ -4,7 +4,7 @@ module "kubewarden_end_to_end_tests_repository" {
 
   name                   = "kubewarden-end-to-end-tests"
   description            = "Files used to run Kubewarden end-to-end tests"
-  teams_with_push_rights = [ data.github_team.kubewarden_developers.id ]
+  teams_with_push_rights = [data.github_team.kubewarden_developers.id]
 
   providers = {
     github = github.kubewarden
@@ -16,7 +16,7 @@ module "kubewarden_github_actions_repository" {
 
   name                   = "github-actions"
   description            = "GitHub actions used by the Kubewarden project"
-  teams_with_push_rights = [ data.github_team.kubewarden_developers.id ]
+  teams_with_push_rights = [data.github_team.kubewarden_developers.id]
 
   providers = {
     github = github.kubewarden
@@ -29,9 +29,9 @@ module "kubewarden_policy_hub_repository" {
 
   name                   = "policy-hub"
   description            = "A place where to find and discover policies"
-  teams_with_push_rights = [ data.github_team.kubewarden_developers.id ]
+  teams_with_push_rights = [data.github_team.kubewarden_developers.id]
   homepage_url           = "https://hub.kubewarden.io"
-  pages                  = [{
+  pages = [{
     source_branch = "gh-pages"
     source_path   = "/"
     cname         = "hub.kubewarden.io"
@@ -45,13 +45,13 @@ module "kubewarden_policy_hub_repository" {
 module "kubewarden_docs_repository" {
   source = "./modules/repository"
 
-  name                   = "docs"
-  description            = "Kubewarden's documentation"
+  name        = "docs"
+  description = "Kubewarden's documentation"
   teams_with_push_rights = [
     data.github_team.kubewarden_developers.id,
-    data.github_team.kubewarden_documentation.id ]
-  homepage_url           = "https://docs.kubewarden.io"
-  pages                  = [{
+  data.github_team.kubewarden_documentation.id]
+  homepage_url = "https://docs.kubewarden.io"
+  pages = [{
     source_branch = "gh-pages"
     source_path   = "/"
     cname         = "docs.kubewarden.io"
@@ -65,16 +65,16 @@ module "kubewarden_docs_repository" {
 module "kubewarden_io_repository" {
   source = "./modules/repository"
 
-  name                   = "kubewarden.io"
-  description            = "Kubewarden website"
+  name        = "kubewarden.io"
+  description = "Kubewarden website"
   teams_with_push_rights = [
     data.github_team.kubewarden_developers.id,
-    data.github_team.kubewarden_documentation.id ]
-  homepage_url           = "https://kubewarden.io"
-  pages                  = [{
+  data.github_team.kubewarden_documentation.id]
+  homepage_url = "https://kubewarden.io"
+  pages = [{
     source_branch = "gh-pages"
     source_path   = "/"
-    cname         = "kubewarden.io"
+    cname         = "www.kubewarden.io"
   }]
 
   providers = {
@@ -85,11 +85,11 @@ module "kubewarden_io_repository" {
 module "kubewarden_rfc_repository" {
   source = "./modules/repository"
 
-  name                   = "rfc"
-  description            = "Kubewarden's RFCs"
-  teams_with_push_rights = [ data.github_team.kubewarden_developers.id,
-                             data.github_team.kubewarden_documentation.id ]
-  homepage_url           = "https://github.com/kubewarden/"
+  name        = "rfc"
+  description = "Kubewarden's RFCs"
+  teams_with_push_rights = [data.github_team.kubewarden_developers.id,
+  data.github_team.kubewarden_documentation.id]
+  homepage_url = "https://github.com/kubewarden/"
 
   providers = {
     github = github.kubewarden
@@ -101,7 +101,7 @@ module "kubewarden_strfmt_repository" {
 
   name                   = "strfmt"
   description            = "A stripped down version of go-openapi/strfrm that works with TinyGo"
-  teams_with_push_rights = [ data.github_team.kubewarden_developers.id ]
+  teams_with_push_rights = [data.github_team.kubewarden_developers.id]
   homepage_url           = "https://github.com/kubewarden/"
 
   providers = {
@@ -114,7 +114,7 @@ module "kubewarden_k8s_objects_repository" {
 
   name                   = "k8s-objects"
   description            = "Experimental: Kubernetes Go types that can be used with TinyGo"
-  teams_with_push_rights = [ data.github_team.kubewarden_developers.id ]
+  teams_with_push_rights = [data.github_team.kubewarden_developers.id]
   homepage_url           = "https://github.com/kubewarden/"
 
   providers = {
@@ -127,7 +127,7 @@ module "kubewarden_k8s_objects_generator_repository" {
 
   name                   = "k8s-objects-generator"
   description            = "CLI tool that generates Kubernetes Go types that can be used with TinyGo starting from the official OpenAPI spec"
-  teams_with_push_rights = [ data.github_team.kubewarden_developers.id ]
+  teams_with_push_rights = [data.github_team.kubewarden_developers.id]
   homepage_url           = "https://github.com/kubewarden/"
 
   providers = {
@@ -140,7 +140,7 @@ module "kubewarden_rancher_kubectl_builder_repository" {
 
   name                   = "rancher-kubectl-builder"
   description            = "Workflow to rebuild and sign rancher/kubectl image"
-  teams_with_push_rights = [ data.github_team.kubewarden_developers.id ]
+  teams_with_push_rights = [data.github_team.kubewarden_developers.id]
   homepage_url           = "https://github.com/kubewarden/"
 
   providers = {
@@ -153,7 +153,7 @@ module "kubewarden_utils_repository" {
 
   name                   = "utils"
   description            = "Utils scripts used by the Kubewarden team and users."
-  teams_with_push_rights = [ data.github_team.kubewarden_developers.id ]
+  teams_with_push_rights = [data.github_team.kubewarden_developers.id]
   homepage_url           = "https://github.com/kubewarden/"
 
   providers = {
@@ -166,7 +166,7 @@ module "kubewarden_load_testing_repository" {
 
   name                   = "load-testing"
   description            = "HTTP load to stress policy-server"
-  teams_with_push_rights = [ data.github_team.kubewarden_developers.id ]
+  teams_with_push_rights = [data.github_team.kubewarden_developers.id]
   homepage_url           = "https://github.com/kubewarden/"
 
   providers = {
@@ -179,7 +179,7 @@ module "kubewarden_fleet_example_repository" {
 
   name                   = "fleet-example"
   description            = "Example of Rancher Fleet bundle for Kubewarden"
-  teams_with_push_rights = [ data.github_team.kubewarden_developers.id ]
+  teams_with_push_rights = [data.github_team.kubewarden_developers.id]
   homepage_url           = "https://github.com/kubewarden/"
 
   providers = {
