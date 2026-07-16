a56af88f9650a1870eb22912742ab308f554e996
Víctor Cuadrado Juan <vcuadradojuan@suse.de>
2026-07-16 16:22:04 +0200

    chore: Allow automerge and secret scanning
    This matches the current repo config of adm-controller and policies.
    
    Signed-off-by: Víctor Cuadrado Juan <vcuadradojuan@suse.de>

--- terraform/repository_core.tf
+++ terraform/repository_core.tf
@@ -8,6 +8,10 @@ module "kubewarden_adm_controller_repository" {
 
   name        = "adm-controller"
   description = "Manage admission policies in your Kubernetes cluster with ease"
+
+  allow_auto_merge                = true
+  secret_scanning_push_protection = "enabled"
+
   extra_topics = [
   ]
   teams_with_push_rights = [data.github_team.kubewarden_developers.id]
